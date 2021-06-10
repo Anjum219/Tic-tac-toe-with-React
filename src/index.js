bc2e0e1f31ce851fc09afebf81325a271ad01f72
Anjum219 <anjumhaz@gmail.com>
2021-06-10 12:49:47 +0600

    Declare winner

--- src/index.js
+++ src/index.js
@@ -24,6 +24,11 @@ class Board extends React.Component {
   }
 
   handleClick(i) {
+    if( calculateWinner(this.state.squares) || this.state.squares[i] ) {  // if there is a winner or 
+                                                                          // this square this already occupied
+      return;
+    }
+
     const newSquares = this.state.squares.slice();
     newSquares[i] = this.state.xIsNext ? 'X' : 'O';
 
@@ -43,7 +48,15 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : '0');
+    let status;
+    const winner = calculateWinner(this.state.squares);
+
+    if(winner) {
+      status = 'Winner: ' + winner;
+    }
+    else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : '0');
+    }
 
     return (
       <div>
@@ -90,4 +103,27 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for( let i = 0; i < lines.length; i++ ) {
+    let [a, b, c] = lines[i];
+
+    if( squares[a] && squares[a] === squares[b] && squares[a] === squares[c] ){
+      return squares[a];
+    }
+  }
+
+  return null;
+}
   
